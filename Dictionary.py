6fcc3062c7831cedee8fa335a84fd99a657e1e30
BillyFarosa <57003140+BillyFarosa@users.noreply.github.com>
2019-12-06 19:21:07 +0700

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,25 +1,35 @@
 kontak = {}
 while True:
     print("")
-    c = input("A)dd, E)dit, D)elete, S)earch, L)ist, Q)uit: ")
-    if c.lower() == 'q':
+    c = input("T)ambah, E)dit, H)apus, C)ari, L)ist, K)eluar: ")
+    if c.lower() == 'k':
         print("CREATE by : BILLY FAROSA")
         break
     elif c.lower() == 'l':
-        print ("\\-------------------------DATA NILAI AKHIR MAHASISWA----------------------------------/")
-        print ("****************************************************************************************")
-        print ()
-        print ()
-        print ("========================================================================================")
-        print (" |  No |   Nama   |     NIM     |  Tugas |   UTS  |   UAS  |  NILAI |" )
-        print ("========================================================================================")
-        i=0
-        for x in kontak.items():
-            i+=1
-            print (" |  {6:2} | {0:7s}  | {1:11} | {2:6d} | {3:6d} | {4:6d} | {5:6.2f} |"\
+        if kontak.items() :
+            print ("\\-------------------------DATA NILAI AKHIR MAHASISWA----------------------------------/")
+            print ("****************************************************************************************")
+            print ()
+            print ()
+            print ("========================================================================================")
+            print (" |  No |   Nama   |     NIM     |  Tugas |   UTS  |   UAS  |  NILAI |" )
+            print ("========================================================================================")
+            i=0
+            for x in kontak.items():
+                i+=1
+                print (" |  {6:2} | {0:7s}  | {1:11} | {2:6d} | {3:6d} | {4:6d} | {5:6.2f} |"\
                    .format(x[0], x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], i))
-        print("========================================================================================")
-    elif c.lower() == 'a':
+        else :
+             print ("\\-------------------------DATA NILAI AKHIR MAHASISWA----------------------------------/")
+             print ("****************************************************************************************")
+             print ()
+             print ()
+             print ("========================================================================================")
+             print (" |  No |   Nama   |     NIM     |  Tugas |   UTS  |   UAS  |  NILAI |" )
+             print ("========================================================================================")
+             print ("                                  Tidak Ada Data")
+             print("========================================================================================")
+    elif c.lower() == 't':
         print(" Tambah Data ")
         nama = input(" Nama: ")
         nim = input(" NIM:")
@@ -36,17 +46,18 @@ while True:
             nilaiuts = int(input (" Nilai UTS     : "))
             nilaiuas = int(input (" Nilai UAS     : "))
             nilaitgs = int(input (" Nilai Tugas   : "))
+            akhir = ((nilaitgs)*30/100 + (nilaiuts)*35/100 + (nilaiuas)*35/100)
             kontak[nama] = nim, nilaiuts, nilaiuas, nilaitgs, akhir
         else:
             print("Kontak {0} tidak ada".format(nama))
-    elif c.lower() == 'd':
+    elif c.lower() == 'h':
         print("Hapus Kontak")
         nama = input("Nama: ")
         if nama in kontak.keys():
             del kontak[nama]
         else:
             print("Kontak {0} tidak ada".format(nama))
-    elif c.lower() == 's':
+    elif c.lower() == 'c':
         print("Search Kontak")
         nama = input("Nama: ")
         if nama in kontak.keys():
